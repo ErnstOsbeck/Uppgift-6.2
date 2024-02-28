ad9686ea5bf0e5bcb6d4f190c7a52f14576447df
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-02-28 10:11:46 +0100

    Add project files.

---
+++ Inlämningsuppgift 6.2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace uppgift_6._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int StörstaTal = Störst(1, 2);
+            Console.WriteLine(StörstaTal);
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else if (tal2 > tal1)
+            {
+                return tal2;
+            }
+            else if (tal1 == tal2)
+            {
+                return tal1;
+            }
+        }
+    }
+}
